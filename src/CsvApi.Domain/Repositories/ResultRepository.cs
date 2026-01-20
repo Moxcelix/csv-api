4eec317091cecd216f2e1d52a3e1a1e3675ba5f6
moxcelix <maxim.spf@gmail.com>
2026-01-20 23:07:59 +0300

    result repo implemented

--- src/CsvApi.Domain/Repositories/ResultRepository.cs
+++ src/CsvApi.Domain/Repositories/ResultRepository.cs
@@ -3,6 +3,7 @@ namespace CsvApi.Domain;
 public interface IResultRepository
 {
     public Result GetResultById(Guid id);
+    public Result GetResultByProcessId(Guid id);
     public void CreateResult(Result result);
     public void UpdateResult(Result result);
 }
---
+++ src/CsvApi.Infrastructure/Repositories/ResultRepository.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using CsvApi.Domain;
+
+namespace CsvApi.Infrastructure;
+
+public class ResultRepository : IResultRepository
+{
+    private readonly AppDbContext _context;
+
+    public ResultRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Result GetResultById(Guid id)
+    {
+        return _context.Results
+            .FirstOrDefault(r => r.Id == id);
+    }
+
+    public Result GetResultByProcessId(Guid id)
+    {
+        return _context.Results
+            .FirstOrDefault(r => r.ProcessId == processId);
+    }
+
+    public void CreateResult(Result result)
+    {
+        _context.Results.Add(result);
+        _context.SaveChanges();
+    }
+
+    public void UpdateResult(Result result)
+    {
+        _context.Results.Update(result);
+        _context.SaveChanges();
+    }
+}
