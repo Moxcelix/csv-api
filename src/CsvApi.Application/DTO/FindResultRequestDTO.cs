21e69801928bcbffaeded68ec51ef176dfe9116c
moxcelix <maxim.spf@gmail.com>
2026-01-21 15:37:45 +0300

    implement find result usecase

--- src/CsvApi.Application/DTO/FindResultRequestDTO.cs
+++ src/CsvApi.Application/DTO/FindResultRequestDTO.cs
@@ -2,11 +2,13 @@ namespace CsvApi.Application;
 
 public struct FindResultRequestDTO
 {
-    public string? Name;
+    public string? name;
     public DateTime? startTime; 
     public DateTime? endTime; 
     public double? minValue;
     public double? maxValue;
     public double? minExecutionTime;
     public double? maxExecutionTime;
+    public int? page;
+    public int? pageSize;
 }
--- src/CsvApi.Application/Usecases/FindResultUsecase.cs
+++ src/CsvApi.Application/Usecases/FindResultUsecase.cs
@@ -2,15 +2,49 @@ namespace CsvApi.Application;
 
 public class FindResultUsecase
 {
-    private readonly IResultRepository _resultRepository;
+    private readonly IFindResultQuery _findQuery;
 
-    public FindResultUsecase(IResultRepository resultRepository)
+    public FindResultUsecase(IFindResultQuery findQuery)
     {
-        _resultRepository = resultRepository;
+        _findQuery = findQuery;
     }
 
     public FindResultResponseDTO Execute(FindResultRequestDTO requestDTO)
     {
-        return new FindResultResponseDTO();
+        var filter = new ResultFilter()
+        {
+            ProcessName = requestDTO.name,
+            FirstOperationTimeStart = requestDTO.startTime,
+            FirstOperationTimeEnd = requestDTO.endTime,
+            MinMeanValue = requestDTO.minValue,
+            MaxMeanValue = requestDTO.maxValue,
+            MinAvgExecutionTime = requestDTO.minExecutionTime,
+            MaxAvgExecutionTime = requestDTO.maxExecutionTime,
+            Page = requestDTO.page,
+            PageSize = requestDTO.PageSize
+        };
+
+        var results = _findQuery.FindByFilter(filter);
+        var resultDTOs = new List<ResultDTO>();
+
+        foreach (var result in results)
+        {
+            resultDTOs.Add(new ResultDTO()
+            {
+                ProcessName = result.Process.Name,
+                DeltaTime = result.Result.DeltaTime,
+                FirstOperationTime = result.Result.FirstOperationTime,
+                AverageExecutionTime = result.Result.AverageExecutionTime,
+                ValueMean = result.Result.ValueMean,
+                ValueMedian = result.Result.ValueMedian,
+                ValueMin = result.Result.ValueMin,
+                ValueMax = result.Result.ValueMax,
+            });
+        }
+
+        return new FindResultResponseDTO()
+        {
+            Results = resultDTOs.ToArray()
+        };
     }
 }
